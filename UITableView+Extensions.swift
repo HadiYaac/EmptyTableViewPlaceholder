4abe3069eb90554dc28fe9b4f3ac2c678a06bc42
HadiYaac <hadi.jacob@gmail.com>
2022-06-10 12:58:00 +0300

    Created UITableView+Extensions swift file

---
+++ UITableView+Extensions.swift
@@ -0,0 +1,73 @@
+//
+//  UITableview+Extensions.swift
+//  Demo
+//
+//  Created by Hadi on 09/06/2022.
+//
+
+import UIKit
+
+
+extension UITableView {
+    
+    func setEmptyView(title: String, message: String, messageImage: UIImage) {
+        
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        
+        let messageImageView = UIImageView()
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        
+        messageImageView.backgroundColor = .clear
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageImageView.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        
+        messageLabel.textColor = UIColor.lightGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageImageView)
+        emptyView.addSubview(messageLabel)
+        
+        messageImageView.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageImageView.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor, constant: -20).isActive = true
+        messageImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        messageImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: messageImageView.bottomAnchor, constant: 10).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        messageLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        
+        messageImageView.image = messageImage
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        
+        messageImageView.shake(0.4)
+        
+        self.backgroundView = emptyView
+        self.separatorStyle = .none
+    }
+    
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
+
+extension UIView {
+    func shake(_ duration: Double? = 0.4) {
+        self.transform = CGAffineTransform(translationX: 20, y: 0)
+        UIView.animate(withDuration: duration ?? 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
+    }
+}
